9ed0c4c69b0cd664e582289c74644402c4753039
Benjamin  Wolbransky <benjamin.wolbransky@ibotta.com>
2022-04-02 14:56:18 -0600

    updated code improved speed

--- get_data.py
+++ get_data.py
@@ -3,25 +3,27 @@ def get_assets():
     ## https://www.kaggle.com/general/74235
     import os
     import subprocess
-    import sys
-    print("")
-    print("This process will pip install Kaggle and download data through Kaggle API.")
-    print("")
-    print("Please confirm that you've downloaded Kaggle JSON credentials into directory")
-    print("")
-    confirm = input("Type 'Y' to continue: ")
-    if confirm.lower() != "y":
-        print("not confirmed. Aborting.")
-        quit()
-    subprocess.run(["pip", "install", "kaggle"])
-    subprocess.run(["kaggle", "competitions", "download", "-c", 'nfl-big-data-bowl-2021'])
+    
+    if os.path.exists('Kaggle-Data-Files/week17.csv'):
+        print('assets previously downloaded')
+    else: 
+        print("This process will pip install Kaggle and download data through Kaggle API.")
+        print("")
+        print("Please confirm that you've downloaded Kaggle JSON credentials into directory")
+        print("")
+        confirm = input("Type 'Y' to continue: ")
+        if confirm.lower() != "y":
+            print("not confirmed. Aborting.")
+            quit()
+        subprocess.run(["pip", "install", "kaggle"])
+        subprocess.run(["kaggle", "competitions", "download", "-c", 'nfl-big-data-bowl-2021'])
 
-    import zipfile
-    cwd = str(os.getcwd())
-    with zipfile.ZipFile(cwd + '/nfl-big-data-bowl-2021.zip', 'r') as zip_ref:
-        zip_ref.extractall(cwd + '/Kaggle-Data-Files')
+        import zipfile
+        cwd = str(os.getcwd())
+        with zipfile.ZipFile(cwd + '/nfl-big-data-bowl-2021.zip', 'r') as zip_ref:
+            zip_ref.extractall(cwd + '/Kaggle-Data-Files')
 
-    print("Data Successfully Downloaded")
+        print("Data Successfully Downloaded")
 
 def get_positional_data():
     import pandas as pd
@@ -32,15 +34,24 @@ def get_positional_data():
     fp = dir + '/full_position.csv'
     cwd = str(os.getcwd())
     if not os.path.exists(dir):
+        print('creating directory')
         os.mkdir(dir)
     if os.path.exists(fp):
+        print('positional data already downloaded.')
+        print('reading positional data.')
         positions = pd.read_csv(fp)
+        print('returning positional data.')
     else:
+        print('reading data')
         positions = pd.DataFrame()
         for week in range(1, 18):
+            print(f'reading week {week}')
             week_data = pd.read_csv(cwd + f'/Kaggle-Data-Files/week{week}.csv')
             week_data['week'] = week
             positions = pd.concat([positions, week_data], axis=0)
+            print(f'added week {week}')
         positions.to_csv(fp, index=False)
+        print(f'positional data written to {fp}')
+        print('returning positional data.')
     
     return positions
